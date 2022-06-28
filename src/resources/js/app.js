98c6be7d091358146ebb878b9e1f27edf3dbe788
kelnid <xopoho2222@gmail.com>
2022-06-28 17:41:11 +0300

    first commit

---
+++ src/resources/js/app.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import PostComponent from "./components/PostComponent";
+
+require('./bootstrap');
+
+const app = new Vue({
+    el: '#app',
+
+    components: {
+        PostComponent,
+    }
+})
