ae5bd2f1dc88160843423efc157ddeb7dd1b4734
kelnid <xopoho2222@gmail.com>
2022-06-30 12:55:12 +0300

    and another one2

---
+++ src/resources/js/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <CreateComponent></CreateComponent>
+        <IndexComponent></IndexComponent>
+    </div>
+</template>
+
+<script>
+import CreateComponent from "./components/CreateComponent";
+import IndexComponent from "./components/IndexComponent";
+
+export default {
+    name: "App",
+
+    data() {
+        return {
+            persons: null
+        }
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+
+    components: {
+        CreateComponent,
+        IndexComponent,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
